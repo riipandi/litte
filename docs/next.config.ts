c69c73511a6cd31301c23aff8500058161349ac4
Aris Ripandi <riipandi@gmail.com>
2025-06-19 18:29:58 +0700

    chore: set docs to SSG

---
+++ docs/next.config.ts
@@ -0,0 +1,16 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  cleanDistDir: true,
+  reactStrictMode: true,
+  poweredByHeader: false,
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "**" }],
+    unoptimized: true,
+  },
+  eslint: { ignoreDuringBuilds: true /* disable eslint on build */ },
+  devIndicators: { position: "bottom-right" },
+};
+
+export default nextConfig;
