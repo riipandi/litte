59e132e48ef5b65d223cf3d4a2233f91415d46c4
Aris Ripandi <riipandi@gmail.com>
2025-10-04 07:06:26 +0700

    feat: initialize Avatar storybook

--- stub/src/component.ts
+++ stub/src/component.ts
@@ -1,5 +1,5 @@
 import { LitteElement } from '@litte/element'
-import { html, nothing } from 'lit'
+import { html /*nothing*/ } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
 import { CHANGEME_COMPONENT_STYLES } from './CHANGEME_COMPONENT_ID.css'
 
